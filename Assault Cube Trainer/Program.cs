5b44587f2b44a166d75ebbb5e22193c7de90e6ed
Misha Sazonov <122310935+justowenRUS@users.noreply.github.com>
2024-03-08 19:20:37 +0300

    1.0 gui

---
+++ Assault Cube Trainer/Program.cs
@@ -0,0 +1,19 @@
+﻿using ClickableTransparentOverlay;
+using ImGuiNET;
+
+namespace AC_Trainer
+{
+    public class Renderer : Overlay
+    {
+        public bool godMode = false;
+        public bool unlimitedAmmo = false;
+
+        protected override void Render()
+        {
+            ImGui.Begin("Menu");
+
+            ImGui.Checkbox("Godmode", ref godMode);
+            ImGui.Checkbox("Unlimited ammo", ref unlimitedAmmo);
+        }
+    }
+}
---
+++ Assault Cube Trainer/Rendered.cs
@@ -0,0 +1,33 @@
+﻿using AC_Trainer;
+using Swed32;
+
+Renderer renderer = new Renderer();
+renderer.Start().Wait();
+
+Swed swed = new Swed("ac_client");
+IntPtr moduleBase = swed.GetModuleBase("ac_client.exe");
+
+IntPtr healthInstructionAddress = moduleBase + 0x1C223;
+IntPtr ammoInstructionAddress = moduleBase + 0xC73EF;
+
+while(true)
+{
+    if (renderer.godMode)
+    {
+        swed.WriteBytes(healthInstructionAddress, "90 90 90");
+    }
+    else
+    {
+        swed.WriteBytes(healthInstructionAddress, "29 7B 04");
+
+    }
+    if (renderer.unlimitedAmmo)
+    {
+        swed.WriteBytes(ammoInstructionAddress, "90 90");
+    }
+    else
+    {
+        swed.WriteBytes(ammoInstructionAddress, "FF 08");
+    }
+    Thread.Sleep(20);
+}
